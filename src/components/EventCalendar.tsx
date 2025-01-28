6b3d24683239dd7ab3b49b369a93c72974ee5a4f
Wesdell <wesdell17@gmail.com>
2025-01-28 10:38:22 -0500

    New event calendar to show incoming events in a calendar view

---
+++ src/components/EventCalendar.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import { events } from "@/mock/data";
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
+export const EventCalendar = () => {
+  const [value, onChange] = useState<Value>(new Date());
+
+  return (
+    <div className="bg-white p-4 rounded-md">
+      <Calendar onChange={onChange} value={value} />
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-semibold my-4">Events</h1>
+        <Image src="/moreDark.png" alt="More dots" width={20} height={20} />
+      </div>
+      <div className="flex flex-col gap-4">
+        {events.map((event) => (
+          <div
+            key={event.id}
+            className="p-5 rounded-md border-2 border-gray-100 border-t-4 odd:border-t-sky even:border-t-purple"
+          >
+            <div className="flex items-center justify-between">
+              <h1 className="font-semibold text-gray-600">{event.title}</h1>
+              <span className="text-xs text-gray-300">{event.time}</span>
+            </div>
+            <p className="mt-2 text-gray-400 text-sm">{event.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
