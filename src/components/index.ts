422fd17293cfd4d725f22e4c164d9ba05ca7ee97
Wesdell <wesdell17@gmail.com>
2025-01-28 10:39:08 -0500

    New announcements component to show incoming events like in card view

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,7 @@
+export * from "./Announcements";
 export * from "./AttendanceChart";
 export * from "./CountChart";
+export * from "./EventCalendar";
 export * from "./FinanceChart";
 export * from "./Menu";
 export * from "./NavBar";
