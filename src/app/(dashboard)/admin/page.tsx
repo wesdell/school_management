6e4290efe3175a6ef19b61f4713ee7e18e11f571
Wesdell <wesdell17@gmail.com>
2025-01-27 20:58:57 -0500

    Markup admin page

---
+++ src/app/(dashboard)/admin/page.tsx
@@ -0,0 +1,33 @@
+import {
+  AttendanceChart,
+  CountChart,
+  FinanceChart,
+  UserCard,
+} from "@/components";
+
+export default function Admin() {
+  return (
+    <div className="flex flex-col gap-4 p-4 md:flex-row">
+      <div className="flex flex-col gap-8 w-full lg:w-2/3">
+        <div className="flex justify-between flex-wrap gap-4">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
+        </div>
+        <div className="flex gap-4 flex-col lg:flex-row">
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+          <div className="w-full lg:w-2/3 h-[450px]">
+            <AttendanceChart />
+          </div>
+        </div>
+        <div className="w-full h-[500px]">
+          <FinanceChart />
+        </div>
+      </div>
+      <div className="w-full lg:w-1/3"></div>
+    </div>
+  );
+}
