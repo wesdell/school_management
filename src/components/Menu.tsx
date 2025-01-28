55a00c4f65f84d9df747b3e00f653908d8791469
Wesdell <wesdell17@gmail.com>
2025-01-28 10:36:58 -0500

    Implement dynamic menu tabs depending in role

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { menuItems } from "@/constants";
+import { role } from "@/mock/data";
 
 export const Menu = () => {
   return (
@@ -10,16 +11,25 @@ export const Menu = () => {
           <span className="hidden lg:block text-gray-400 font-light my-4">
             {menuItem.title}
           </span>
-          {menuItem.items.map((item) => (
-            <Link
-              href={item.href}
-              key={item.label}
-              className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 my-2"
-            >
-              <Image src={item.icon} alt={item.label} width={20} height={20} />
-              <span className="hidden lg:block">{item.label}</span>
-            </Link>
-          ))}
+          {menuItem.items.map((item) => {
+            if (item.visible.includes(role)) {
+              return (
+                <Link
+                  href={item.href}
+                  key={item.label}
+                  className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-skyLight"
+                >
+                  <Image
+                    src={item.icon}
+                    alt={item.label}
+                    width={20}
+                    height={20}
+                  />
+                  <span className="hidden lg:block">{item.label}</span>
+                </Link>
+              );
+            }
+          })}
         </div>
       ))}
     </div>
