d1303e49f6db5bdab3f5d0debd9ab82ce3570af9
Wesdell <wesdell17@gmail.com>
2025-01-28 20:17:53 -0500

    Markup teacher page

---
+++ src/app/(dashboard)/teacher/page.tsx
@@ -0,0 +1,17 @@
+import { Announcements, BigCalendar } from "@/components";
+
+export default function Teacher() {
+  return (
+    <div className="flex-1 flex flex-col gap-4 p-4 xl:flex-row">
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Schedule</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      <div className="flex flex-col gap-8 w-full xl:w-1/3">
+        <Announcements />
+      </div>
+    </div>
+  );
+}
