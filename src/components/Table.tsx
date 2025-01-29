fee9e071a7aa111a74563bdfb8c5a05c59c2e848
Wesdell <wesdell17@gmail.com>
2025-01-29 18:43:07 -0500

    New table component to display different fields in a table

---
+++ src/components/Table.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+interface Props {
+  columns: { header: string; accessor: string; className?: string }[];
+  data: any[];
+  renderRow: (item: any) => React.ReactNode;
+}
+
+export const Table = ({ columns, data, renderRow }: Props) => {
+  return (
+    <table className="w-full mt-4">
+      <thead>
+        <tr className="text-left text-sm text-gray-500">
+          {columns.map((column) => (
+            <th key={column.accessor} className={column.className}>
+              {column.header}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>{data.map((item) => renderRow(item))}</tbody>
+    </table>
+  );
+};
