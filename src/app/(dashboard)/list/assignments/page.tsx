c371e180e05f9b08feec7146086b1c77fb848ea3
Wesdell <wesdell17@gmail.com>
2025-01-31 20:19:56 -0500

    New page to display all assignments and its data

---
+++ src/app/(dashboard)/list/assignments/page.tsx
@@ -0,0 +1,93 @@
+import Image from "next/image";
+import Link from "next/link";
+import { role, assignmentsData } from "@/mock/data";
+import { Pagination, Table, TableSearch } from "@/components";
+
+type Assignment = {
+  id: number;
+  subject: string;
+  class: string;
+  teacher: string;
+  dueDate: string;
+};
+
+const columns = [
+  { header: "Subject", accessor: "subject" },
+  { header: "Class", accessor: "class" },
+  {
+    header: "Teacher",
+    accessor: "teacher",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Due Date",
+    accessor: "dueDate",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Actions",
+    accessor: "actions",
+  },
+];
+
+export default function ListAssignments() {
+  const renderRow = (item: Assignment) => (
+    <tr
+      key={item.id}
+      className="text-sm border-b border-gray-200 even:bg-slate-50 hover:bg-purpleLight"
+    >
+      <td className="flex items-center gap-4 p-4">{item.subject}</td>
+      <td>{item.class}</td>
+      <td className="hidden md:table-cell">{item.teacher}</td>
+      <td className="hidden md:table-cell">{item.dueDate}</td>
+      <td>
+        <div className="flex items-center gap-2">
+          <Link href={`/list/assignments/${item.id}`}>
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-sky">
+              <Image src="/edit.png" alt="View action" width={16} height={16} />
+            </button>
+          </Link>
+          {role === "admin" && (
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-purple">
+              <Image
+                src="/delete.png"
+                alt="Delete action"
+                width={16}
+                height={16}
+              />
+            </button>
+          )}
+        </div>
+      </td>
+    </tr>
+  );
+
+  return (
+    <div className="flex-1 p-4 bg-white rounded-md m-4 mt-0">
+      <div className="flex items-center justify-between">
+        <h1 className="hidden md:block text-lg font-semibold">
+          All assignments
+        </h1>
+        <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+          <TableSearch />
+          <div className="flex items-center gap-4 self-end">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow">
+              <Image src="/filter.png" alt="Filter" width={14} height={14} />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow">
+              <Image src="/sort.png" alt="Filter" width={14} height={14} />
+            </button>
+            {role === "admin" && (
+              <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow">
+                <Image src="/plus.png" alt="Filter" width={14} height={14} />
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+      <div></div>
+      <Table columns={columns} data={assignmentsData} renderRow={renderRow} />
+      <Pagination />
+    </div>
+  );
+}
