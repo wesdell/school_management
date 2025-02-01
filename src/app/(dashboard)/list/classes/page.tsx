97ef19a6771ca45ef0ce1498045affdc4f148016
Wesdell <wesdell17@gmail.com>
2025-01-31 20:17:44 -0500

    New page to display all classes and its data

---
+++ src/app/(dashboard)/list/classes/page.tsx
@@ -0,0 +1,95 @@
+import Image from "next/image";
+import Link from "next/link";
+import { role, classesData } from "@/mock/data";
+import { Pagination, Table, TableSearch } from "@/components";
+
+type Class = {
+  id: number;
+  name: string;
+  capacity: number;
+  grade: number;
+  supervisor: string;
+};
+
+const columns = [
+  { header: "Class", accessor: "class" },
+  {
+    header: "Capacity",
+    accessor: "capacity",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Grade",
+    accessor: "grade",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Supervisor",
+    accessor: "supervisor",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Actions",
+    accessor: "actions",
+  },
+];
+
+export default function ListClasses() {
+  const renderRow = (item: Class) => (
+    <tr
+      key={item.id}
+      className="text-sm border-b border-gray-200 even:bg-slate-50 hover:bg-purpleLight"
+    >
+      <td className="flex items-center gap-4 p-4">{item.name}</td>
+      <td className="hidden md:table-cell">{item.capacity}</td>
+      <td className="hidden md:table-cell">{item.grade}</td>
+      <td className="hidden md:table-cell">{item.supervisor}</td>
+      <td>
+        <div className="flex items-center gap-2">
+          <Link href={`/list/classes/${item.id}`}>
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-sky">
+              <Image src="/edit.png" alt="View action" width={16} height={16} />
+            </button>
+          </Link>
+          {role === "admin" && (
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-purple">
+              <Image
+                src="/delete.png"
+                alt="Delete action"
+                width={16}
+                height={16}
+              />
+            </button>
+          )}
+        </div>
+      </td>
+    </tr>
+  );
+
+  return (
+    <div className="flex-1 p-4 bg-white rounded-md m-4 mt-0">
+      <div className="flex items-center justify-between">
+        <h1 className="hidden md:block text-lg font-semibold">All classes</h1>
+        <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+          <TableSearch />
+          <div className="flex items-center gap-4 self-end">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow">
+              <Image src="/filter.png" alt="Filter" width={14} height={14} />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow">
+              <Image src="/sort.png" alt="Filter" width={14} height={14} />
+            </button>
+            {role === "admin" && (
+              <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow">
+                <Image src="/plus.png" alt="Filter" width={14} height={14} />
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+      <div></div>
+      <Table columns={columns} data={classesData} renderRow={renderRow} />
+      <Pagination />
+    </div>
+  );
+}
