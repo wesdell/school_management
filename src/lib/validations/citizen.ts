b215351205f9a05c76068d051fccce66fb2e9b17
Wesdell <wesdell17@gmail.com>
2025-02-01 20:40:02 -0500

    New validation schema for personal information

---
+++ src/lib/validations/citizen.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+export const CitizenValidation = z.object({
+  username: z
+    .string()
+    .min(3, { message: "Username must be at least 3 characters long!" })
+    .max(20, { message: "Username must be at most 20 characters long!" }),
+  email: z.string().email({ message: "Invalid email address!" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long!" }),
+  firstName: z.string().min(1, { message: "First name is required!" }),
+  lastName: z.string().min(1, { message: "Last name is required!" }),
+  phone: z.string().min(1, { message: "Phone is required!" }),
+  address: z.string().min(1, { message: "Address is required!" }),
+  bloodType: z.string().min(1, { message: "Blood Type is required!" }),
+  birthday: z.date({ message: "Birthday is required!" }),
+  sex: z.enum(["male", "female"], { message: "Sex is required!" }),
+  img: z.instanceof(File, { message: "Image is required" }),
+});
