668241d4f3e9083d76b3941791bafb9b9ff63fe1
Wesdell <wesdell17@gmail.com>
2025-02-09 13:03:09 -0500

    Fetch announcements data from docker db container, enable pagination and filter by url search params

--- src/app/(dashboard)/list/announcements/page.tsx
+++ src/app/(dashboard)/list/announcements/page.tsx
@@ -1,12 +1,10 @@
-import { announcementsData, role } from "@/mock/data";
-import { FormModal, Table } from "@/components";
+import { Announcement, Class, Prisma } from "@prisma/client";
+import prisma from "@/lib/prisma";
+import { role } from "@/mock/data";
+import { RECORDS_PER_PAGE } from "@/constants";
+import { FormModal, Pagination, Table } from "@/components";
 
-type Announcement = {
-  id: number;
-  title: string;
-  class: string;
-  date: string;
-};
+type AnnouncementList = Announcement & { class: Class };
 
 const columns = [
   { header: "Title", accessor: "title" },
@@ -22,29 +20,70 @@ const columns = [
   },
 ];
 
-export default function ListAnnouncements() {
-  const renderRow = (item: Announcement) => (
-    <tr
-      key={item.id}
-      className="text-sm border-b border-gray-200 even:bg-slate-50 hover:bg-purpleLight"
-    >
-      <td className="flex items-center gap-4 p-4">{item.title}</td>
-      <td>{item.class}</td>
-      <td className="hidden md:table-cell">{item.date}</td>
-      <td>
-        <div className="flex items-center gap-2">
-          {role === "admin" && (
-            <>
-              <FormModal table="announcement" type="update" data={item} />
-              <FormModal table="announcement" type="delete" id={item.id} />
-            </>
-          )}
-        </div>
-      </td>
-    </tr>
-  );
+const renderRow = (item: AnnouncementList) => (
+  <tr
+    key={item.id}
+    className="text-sm border-b border-gray-200 even:bg-slate-50 hover:bg-purpleLight"
+  >
+    <td className="flex items-center gap-4 p-4">{item.title}</td>
+    <td>{item.class.name}</td>
+    <td className="hidden md:table-cell">
+      {new Intl.DateTimeFormat("en-US").format(item.date)}
+    </td>
+    <td>
+      <div className="flex items-center gap-2">
+        {role === "admin" && (
+          <>
+            <FormModal table="announcement" type="update" data={item} />
+            <FormModal table="announcement" type="delete" id={item.id} />
+          </>
+        )}
+      </div>
+    </td>
+  </tr>
+);
+
+export default async function ListAnnouncements({
+  searchParams,
+}: {
+  searchParams: { [_: string]: string | undefined };
+}) {
+  const { page, ...params } = await searchParams;
+  const actualPage = page ? parseInt(page) : 1;
+
+  const query: Prisma.AnnouncementWhereInput = {};
+  if (params) {
+    for (const [key, value] of Object.entries(params)) {
+      if (value !== undefined) {
+        switch (key) {
+          case "search": {
+            query.title = { contains: value, mode: "insensitive" };
+            break;
+          }
+          default: {
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  const [announcements, count] = await prisma.$transaction([
+    prisma.announcement.findMany({
+      where: query,
+      include: { class: true },
+      take: RECORDS_PER_PAGE,
+      skip: RECORDS_PER_PAGE * (actualPage - 1),
+    }),
+    prisma.announcement.count({
+      where: query,
+    }),
+  ]);
 
   return (
-    <Table columns={columns} data={announcementsData} renderRow={renderRow} />
+    <>
+      <Table columns={columns} data={announcements} renderRow={renderRow} />
+      <Pagination page={actualPage} count={count} />
+    </>
   );
 }
