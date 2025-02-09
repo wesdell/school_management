e6a8eabbf67425e84261edd048a9b4db0821582c
Wesdell <wesdell17@gmail.com>
2025-02-09 13:04:14 -0500

    Fetch exams data from docker db container, enable pagination and filter by url search params

--- src/app/(dashboard)/list/exams/page.tsx
+++ src/app/(dashboard)/list/exams/page.tsx
@@ -1,12 +1,11 @@
-import { examsData, role } from "@/mock/data";
-import { FormModal, Table } from "@/components";
+import { Class, Exam, Prisma, Subject, Teacher } from "@prisma/client";
+import prisma from "@/lib/prisma";
+import { role } from "@/mock/data";
+import { RECORDS_PER_PAGE } from "@/constants";
+import { FormModal, Pagination, Table } from "@/components";
 
-type Exam = {
-  id: number;
-  subject: string;
-  class: string;
-  teacher: string;
-  date: string;
+type ExamList = Exam & {
+  lesson: { class: Class; subject: Subject; teacher: Teacher };
 };
 
 const columns = [
@@ -28,28 +27,91 @@ const columns = [
   },
 ];
 
-export default function ListExams() {
-  const renderRow = (item: Exam) => (
-    <tr
-      key={item.id}
-      className="text-sm border-b border-gray-200 even:bg-slate-50 hover:bg-purpleLight"
-    >
-      <td className="flex items-center gap-4 p-4">{item.subject}</td>
-      <td>{item.class}</td>
-      <td className="hidden md:table-cell">{item.teacher}</td>
-      <td className="hidden md:table-cell">{item.date}</td>
-      <td>
-        <div className="flex items-center gap-2">
-          {role === "admin" && (
-            <>
-              <FormModal table="exam" type="update" data={item} />
-              <FormModal table="exam" type="delete" id={item.id} />
-            </>
-          )}
-        </div>
-      </td>
-    </tr>
-  );
+const renderRow = (item: ExamList) => (
+  <tr
+    key={item.id}
+    className="text-sm border-b border-gray-200 even:bg-slate-50 hover:bg-purpleLight"
+  >
+    <td className="flex items-center gap-4 p-4">{item.lesson.subject.name}</td>
+    <td>{item.lesson.class.name}</td>
+    <td className="hidden md:table-cell">{`${item.lesson.teacher.name} ${item.lesson.teacher.surname}`}</td>
+    <td className="hidden md:table-cell">
+      {new Intl.DateTimeFormat("en-US").format(item.startTime)}
+    </td>
+    <td>
+      <div className="flex items-center gap-2">
+        {role === "admin" && (
+          <>
+            <FormModal table="exam" type="update" data={item} />
+            <FormModal table="exam" type="delete" id={item.id} />
+          </>
+        )}
+      </div>
+    </td>
+  </tr>
+);
 
-  return <Table columns={columns} data={examsData} renderRow={renderRow} />;
+export default async function ListExams({
+  searchParams,
+}: {
+  searchParams: { [_: string]: string | undefined };
+}) {
+  const { page, ...params } = await searchParams;
+  const actualPage = page ? parseInt(page) : 1;
+
+  const query: Prisma.ExamWhereInput = {};
+  if (params) {
+    for (const [key, value] of Object.entries(params)) {
+      if (value !== undefined) {
+        switch (key) {
+          case "classId": {
+            query.lesson = { classId: parseInt(value) };
+            break;
+          }
+          case "teacherId": {
+            query.lesson = { teacherId: value };
+            break;
+          }
+          case "search": {
+            query.lesson = {
+              subject: {
+                name: { contains: value, mode: "insensitive" },
+              },
+            };
+            break;
+          }
+          default: {
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  const [exams, count] = await prisma.$transaction([
+    prisma.exam.findMany({
+      where: query,
+      include: {
+        lesson: {
+          select: {
+            subject: { select: { name: true } },
+            class: { select: { name: true } },
+            teacher: { select: { name: true, surname: true } },
+          },
+        },
+      },
+      take: RECORDS_PER_PAGE,
+      skip: RECORDS_PER_PAGE * (actualPage - 1),
+    }),
+    prisma.exam.count({
+      where: query,
+    }),
+  ]);
+
+  return (
+    <>
+      <Table columns={columns} data={exams} renderRow={renderRow} />
+      <Pagination page={actualPage} count={count} />
+    </>
+  );
 }
