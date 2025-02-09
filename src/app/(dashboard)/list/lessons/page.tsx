b0a960a88d1746a61fd292f01a5ca6db459c7147
Wesdell <wesdell17@gmail.com>
2025-02-09 13:04:39 -0500

    Fetch lessons data from docker db container, enable pagination and filter by url search params

--- src/app/(dashboard)/list/lessons/page.tsx
+++ src/app/(dashboard)/list/lessons/page.tsx
@@ -1,11 +1,11 @@
-import { lessonsData, role } from "@/mock/data";
-import { FormModal, Table } from "@/components";
+import { Class, Lesson, Prisma, Subject, Teacher } from "@prisma/client";
+import prisma from "@/lib/prisma";
+import { role } from "@/mock/data";
+import { RECORDS_PER_PAGE } from "@/constants";
+import { FormModal, Pagination, Table } from "@/components";
 
-type Lesson = {
-  id: number;
-  subject: string;
-  class: string;
-  teacher: string;
+type LessonList = Lesson & { subject: Subject } & { teacher: Teacher } & {
+  class: Class;
 };
 
 const columns = [
@@ -22,27 +22,83 @@ const columns = [
   },
 ];
 
-export default function ListLessons() {
-  const renderRow = (item: Lesson) => (
-    <tr
-      key={item.id}
-      className="text-sm border-b border-gray-200 even:bg-slate-50 hover:bg-purpleLight"
-    >
-      <td className="flex items-center gap-4 p-4">{item.subject}</td>
-      <td>{item.class}</td>
-      <td className="hidden md:table-cell">{item.teacher}</td>
-      <td>
-        <div className="flex items-center gap-2">
-          {role === "admin" && (
-            <>
-              <FormModal table="lesson" type="update" data={item} />
-              <FormModal table="lesson" type="delete" id={item.id} />
-            </>
-          )}
-        </div>
-      </td>
-    </tr>
-  );
+const renderRow = (item: LessonList) => (
+  <tr
+    key={item.id}
+    className="text-sm border-b border-gray-200 even:bg-slate-50 hover:bg-purpleLight"
+  >
+    <td className="flex items-center gap-4 p-4">{item.subject.name}</td>
+    <td>{item.class.name}</td>
+    <td className="hidden md:table-cell">{`${item.teacher.name} ${item.teacher.surname}`}</td>
+    <td>
+      <div className="flex items-center gap-2">
+        {role === "admin" && (
+          <>
+            <FormModal table="lesson" type="update" data={item} />
+            <FormModal table="lesson" type="delete" id={item.id} />
+          </>
+        )}
+      </div>
+    </td>
+  </tr>
+);
 
-  return <Table columns={columns} data={lessonsData} renderRow={renderRow} />;
+export default async function ListLessons({
+  searchParams,
+}: {
+  searchParams: { [_: string]: string | undefined };
+}) {
+  const { page, ...params } = await searchParams;
+  const actualPage = page ? parseInt(page) : 1;
+
+  const query: Prisma.LessonWhereInput = {};
+  if (params) {
+    for (const [key, value] of Object.entries(params)) {
+      if (value !== undefined) {
+        switch (key) {
+          case "classId": {
+            query.classId = parseInt(value);
+            break;
+          }
+          case "teacherId": {
+            query.teacherId = value;
+            break;
+          }
+          case "search": {
+            query.OR = [
+              { subject: { name: { contains: value, mode: "insensitive" } } },
+              { teacher: { name: { contains: value, mode: "insensitive" } } },
+            ];
+            break;
+          }
+          default: {
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  const [lessons, count] = await prisma.$transaction([
+    prisma.lesson.findMany({
+      where: query,
+      include: {
+        subject: { select: { name: true } },
+        class: { select: { name: true } },
+        teacher: { select: { name: true, surname: true } },
+      },
+      take: RECORDS_PER_PAGE,
+      skip: RECORDS_PER_PAGE * (actualPage - 1),
+    }),
+    prisma.lesson.count({
+      where: query,
+    }),
+  ]);
+
+  return (
+    <>
+      <Table columns={columns} data={lessons} renderRow={renderRow} />
+      <Pagination page={actualPage} count={count} />
+    </>
+  );
 }
