a83cbef347609d002befa768f69e9f49613aa4bb
Wesdell <wesdell17@gmail.com>
2025-02-21 10:38:14 -0500

    Fetch records from docker db

--- src/components/cards/UserCard.tsx
+++ src/components/cards/UserCard.tsx
@@ -1,6 +1,19 @@
 import Image from "next/image";
+import prisma from "@/lib/prisma";
 
-export const UserCard = ({ type }: { type: string }) => {
+interface Props {
+  type: "admin" | "student" | "teacher" | "parent";
+}
+
+export const UserCard = async ({ type }: Props) => {
+  const modelMap: Record<typeof type, any> = {
+    admin: prisma.admin,
+    teacher: prisma.teacher,
+    student: prisma.student,
+    parent: prisma.parent,
+  };
+  const records = await modelMap[type].count();
+
   return (
     <div className="rounded-2xl odd:bg-purple even:bg-yellow p-4 flex-1 min-w-[130px]">
       <div className="flex justify-between items-center">
@@ -9,7 +22,7 @@ export const UserCard = ({ type }: { type: string }) => {
         </span>
         <Image src="/more.png" alt="More dots" width={20} height={20} />
       </div>
-      <h1 className="text-2xl font-semibold my-4">1,234</h1>
+      <h1 className="text-2xl font-semibold my-4">{records}</h1>
       <h2 className="capitalize text-sm font-medium text-gray-500">{type}s</h2>
     </div>
   );
