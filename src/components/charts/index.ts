a49dce91db98e2d323f26b6bf6077cb216502efc
Wesdell <wesdell17@gmail.com>
2025-02-21 11:29:49 -0500

    Extract attendance chart container markup and fetch students status

--- src/components/charts/index.ts
+++ src/components/charts/index.ts
@@ -1,4 +1,5 @@
 export * from "./AttendanceChart";
+export * from "./AttendanceChartContainer";
 export * from "./CountChart";
 export * from "./CountChartContainer";
 export * from "./FinanceChart";
