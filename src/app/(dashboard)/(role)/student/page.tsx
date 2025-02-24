47bae54ef5d76ebd199d00674c8abb49cae94d22
Wesdell <wesdell17@gmail.com>
2025-02-24 11:37:04 -0500

    Fetch role page data dpeneding on its user id

--- src/app/(dashboard)/(role)/student/page.tsx
+++ src/app/(dashboard)/(role)/student/page.tsx
@@ -1,3 +1,18 @@
-export default function Student() {
-  return <h1 className="text-xl font-semibold">Schedule (A4)</h1>;
+import React from "react";
+import prisma from "@/lib/prisma";
+import { useUserId } from "@/hooks";
+import { BigCalendarContainer } from "@/components";
+
+export default async function Student() {
+  const { userId } = await useUserId();
+  const classItem = await prisma.class.findMany({
+    where: { students: { some: { id: userId! } } },
+  });
+
+  return (
+    <>
+      <h1 className="text-xl font-semibold">Schedule (A4)</h1>
+      <BigCalendarContainer type="classId" id={classItem[0].id} />
+    </>
+  );
 }
--- src/app/(dashboard)/(role)/teacher/page.tsx
+++ src/app/(dashboard)/(role)/teacher/page.tsx
@@ -1,3 +1,14 @@
-export default function Teacher() {
-  return <h1 className="text-xl font-semibold">Schedule</h1>;
+import React from "react";
+import { useUserId } from "@/hooks";
+import { BigCalendarContainer } from "@/components";
+
+export default async function Teacher() {
+  const { userId } = await useUserId();
+
+  return (
+    <>
+      <h1 className="text-xl font-semibold">Schedule</h1>
+      <BigCalendarContainer type="teacherId" id={userId!} />
+    </>
+  );
 }
